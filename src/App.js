967357a7c0d3714867026e1ea2b2e05a00071a80
cem-sucu <cems.cems.17@gmail.com>
2022-07-29 16:02:34 +0200

    reac memo & portal création d'une modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
+import Modal from "./Modal";
 import "./App.css";
-import ParentComponent from "./ParentComponent";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showModal: false,
+        };
+    }
+
+    handleShow = () => {
+        this.setState({
+            showModal: true,
+        });
+    };
+
+    handleHide = () => {
+        this.setState({
+            showModal: false,
+        });
+    };
+
     render() {
+        const modal = this.state.showModal && <Modal close={this.handleHide} />;
+
         return (
             <div className="App">
-                <ParentComponent />
+                <button onClick={this.handleShow}>Afficher le Modal</button>
+                {modal}
             </div>
         );
     }
