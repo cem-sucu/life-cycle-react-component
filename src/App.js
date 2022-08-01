8eabf2a95cfa43e3f1df94122b51498a49a6e9ce
cem-sucu <cems.cems.17@gmail.com>
2022-08-01 09:18:46 +0200

    refs bug fixer

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "./App.css";
 import MyRef from "./MyRef";
 
 class App extends Component {
-
     constructor(props) {
-      super(props)
-    
+        super(props);
+
         this.refComponent = React.createRef();
     }
+
+    handleClick = () => {
+        this.refComponent.current.addFocus();
+    };
+
     render() {
         return (
             <div className="App">
-                <MyRef ref={this.refComponent}/>
-                <button onClick={handleClick}>Valider</button>
+                <MyRef ref={this.refComponent} />
+                <button onClick={this.handleClick}>Valider</button>
             </div>
         );
     }
